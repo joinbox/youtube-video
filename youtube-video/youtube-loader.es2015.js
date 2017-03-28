15d4a041496811bd0f3432d3ded9fa38eecb3909
Felix Steiner <felix_steiner@bluewin.ch>
2017-03-28 15:22:32 +0200

    Initial commit

---
+++ youtube-video/youtube-loader.es2015.js
@@ -0,0 +1,95 @@
+(() => {
+
+	/* global document, window */
+
+	// Set to true as soon as we start loading; we don't want to have multiple 
+	// script tags with the same source in the DOM
+	let _isLoading = false;
+
+	// Set to true as soon as the ready callback (onYouTubeIframeAPIReady) is fired
+	let _isReady = false;
+
+	// Callbacks registered through registerCallback
+	const _callbacks = [];
+
+	/**
+	* Simple that loads the YouTube iFrame API. 
+	* See https://developers.google.com/youtube/iframe_api_reference.
+	*
+	* Use like: 
+	* YoutubeLoader.registerCallback(() => {});
+	* YoutubeLoader.load();
+	*/
+	window.YouTubeLoader = {
+
+		/**
+		* Loads the YouTube iFrame API
+		*/
+		load: function() {
+
+			console.log('YouTubeLoader: Load YouTube API');
+
+			if (_isLoading) return;
+
+			_isLoading = true;
+
+			// Make primary callback available globally
+			window.onYouTubeIframeAPIReady = this._loaded.bind(this);
+			const tag = document.createElement('script');
+			tag.setAttribute('src', 'https://www.youtube.com/iframe_api');
+
+			// 1st Prio: Insert before first script tag
+			const firstScriptTag = document.getElementsByTagName('script')[0];
+			if (firstScriptTag) {
+				firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+				return;
+			}
+
+			// 2nd Prio: Insert before last element in head
+			const head = document.getElementsByTagName('head');
+			if (head) {
+				head.appendChild(tag);
+				return;
+			}
+
+			const error = new Error('YouTubeLoader: No insertion point found; make sure your site has a <head> tag.');
+			console.error(error);
+			throw(error);
+
+
+		}
+
+
+		/**
+		* Called through window.onYouTubeIframeAPIReady when YouTube API was loaded.
+		*/
+		, _loaded: function() {
+
+			console.log('YouTubeLoader: API Loaded, call %o callbacks', _callbacks.length);
+			this._isReady = true;
+			_callbacks.forEach((callback) => callback());
+
+		}
+
+
+		/**
+		* Register a function that will be called as soon as the YouTube API is ready
+		* @param {Function} callback
+		*/
+		, registerCallback: function(callback) {
+
+			console.log('YouTubeLoader: Callback registered');
+
+			if (_isReady) {
+				console.log('YouTubeLoader: Directly call callback, API is ready');
+				callback();
+				return;
+			}
+
+			_callbacks.push(callback);
+
+		}
+
+
+	};
+})();
---
+++ youtube-video/youtube-video.es2015.js
@@ -0,0 +1,154 @@
+(() => {
+
+	/* global YouTubeLoader, YT, window, document, HTMLElement, CustomEvent */
+
+	class YouTubeVideo extends HTMLElement {
+
+		constructor() {
+			super();
+		}
+
+
+		connectedCallback() {
+
+			const identifier = this.getAttribute('identifier');
+			console.log('YouTubeVideo: Load video for identifier %o', identifier);
+			if (!identifier) console.warn('YouTubeVideo: Identifier is missing on %o', this);
+			else {
+				this._identifier = identifier;
+				this._setupVideo();
+			}
+
+			this._emitEvent('add-video', { element: this });
+
+		}
+
+
+
+		/**
+		* Load YouTube API (through YouTubeLoader, register callback for initialization
+		*/
+		_setupVideo() {
+
+			YouTubeLoader.load();
+			YouTubeLoader.registerCallback(() => {
+				this._replaceVideo();
+			});
+
+		}
+
+
+		/**
+		* Replace placeholder with video. Setup all event listeners. Store reference to 
+		* YouTube Player in this._player.
+		*/
+		_replaceVideo() {
+			
+			// Create a temporary child that will be replaced by the video
+			const temporaryChild = document.createElement('div');
+			this.appendChild(temporaryChild);
+
+			// Create options that we pass to the player
+			const options = {
+				videoId				: this._identifier
+				, events			: {
+					onReady			: () => this._emitEvent('ready')
+					, onStateChange	: (ev) => {
+						if (ev.data === YT.PlayerState.ENDED) this._emitEvent('end');
+						else if (ev.data === YT.PlayerState.PLAYING) this._emitEvent('play');
+						else if (ev.data === YT.PlayerState.PAUSED) this._emitEvent('pause');
+					}
+				}
+			};
+
+
+			// Get playerVars from attribute
+			const playerVars = this._getPlayerVars();
+			if (playerVars) options.playerVars = playerVars;
+
+			// Setup and store player
+			this._player = new YT.Player(temporaryChild, options);
+
+		}
+
+
+
+		/**
+		* Get and validate attribute player-vars, return vars if set, else undefined. 
+		* @returns {Object|undefined}
+		*/
+		_getPlayerVars() {
+
+			if (!this.getAttribute('player-vars')) return;
+
+			let playerVars;
+			try {
+				playerVars = JSON.parse(this.getAttribute('player-vars'));
+			}
+			catch(err) {
+				console.error('YouTubeVideo: Could not set player-vars, value provided was invalid: %o', err);
+				return;
+			}
+
+			if (!playerVars || typeof playerVars !== 'object') {
+				console.error('YouTubeVideo: Invalid playerVars, must be an object: %o', playerVars);
+				return;
+			}
+
+			return playerVars;
+
+		}
+
+
+
+
+		/***
+		* Play video
+		*/
+		play() {
+			console.log('YouTubeVideo: Play %o', this._identifier);
+			if (this._player) this._player.playVideo();
+			else console.warn('YouTubeVideo: Cannot play video until it\'s ready. Listen to the ready event before playing a video');
+		}
+
+
+
+
+		/***
+		* Pause video
+		*/
+		pause() {
+			console.log('YouTubeVideo: Pause %o', this._identifier);
+			if (this._player) this._player.pauseVideo();
+			else console.warn('YouTubeVideo: Cannot pause video until it\'s ready. Listen to the ready event before pausing a video');
+		}
+
+
+		/**
+		* @returns {Boolean}
+		*/
+		isReady() {
+			return !!this._player;
+		}
+
+
+		/**
+		* Simply emits events
+		* @param {String} type		Type of callbacks to execute
+		* @param {Any} args			Arguments to pass to the callback
+		*/
+		_emitEvent(type, args) {
+			console.log('YouTubeVideo: Emit event %o', type);
+			const event = new CustomEvent(type, { detail: args, bubbles: true });
+			this.dispatchEvent(event);
+		}
+
+
+	}
+
+	window.YouTubeVideo = YouTubeVideo;
+	window.customElements.define('youtube-video', YouTubeVideo);
+
+})();
+
+
